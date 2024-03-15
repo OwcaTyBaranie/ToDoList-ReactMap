a350cdb5ac65e1492172a7d0129722a7bc257555
Piotr Wawrzyniak <hola.wawrzyniak@gmail.com>
2024-03-15 23:36:36 +0100

    Add columns and icons

--- react-app/src/components/List/List.js
+++ react-app/src/components/List/List.js
@@ -1,21 +1,18 @@
+import styles from './List.module.scss';
+import Column from '../Column/Column';
 const List = () => {
     return (
-        <header>
-          Things to do<span>soon</span>
-          <p>Interesting things I want to check out</p>
-          <section>
-            <article>
-                <h2>Books</h2>
-            </article>
-            <article>
-                <h2>Movies</h2>
-            </article>
-            <article>
-                <h2>Games</h2>
-            </article>
+      <div className={styles.list}>
+          <header className={styles.header}>
+              <h2 className={styles.title}>Things to do<span>soon!</span></h2>
+          </header>
+          <p className={styles.description}>Interesting things I want to check out</p>
+          <section className={styles.columns}>
+        <Column icon="book" title="Books"/>
+        <Column icon="film" title="Movies"/>
+        <Column icon="gamepad" title="Games"/>
           </section>
-        </header>
+      </div>
     );
-
-}
+  };
 export default List;
