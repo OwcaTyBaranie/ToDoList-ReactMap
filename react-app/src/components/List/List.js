a9a641038d202be0c21847bc3a4ef08a582fbdf1
Piotr Wawrzyniak <hola.wawrzyniak@gmail.com>
2024-03-15 14:33:01 +0100

    Add new components Hero, List, SearchFrom, install SASS

---
+++ react-app/src/components/List/List.js
@@ -0,0 +1,21 @@
+const List = () => {
+    return (
+        <header>
+          Things to do<span>soon</span>
+          <p>Interesting things I want to check out</p>
+          <section>
+            <article>
+                <h2>Books</h2>
+            </article>
+            <article>
+                <h2>Movies</h2>
+            </article>
+            <article>
+                <h2>Games</h2>
+            </article>
+          </section>
+        </header>
+    );
+
+}
+export default List;
